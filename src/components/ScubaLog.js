591a9420c3b0212aceb97aeb1938bd66c9c2fcaf
Johnson4996 <Johnson.4996@gmail.com>
2020-09-14 14:26:17 -0400

    added auth and is working

--- src/components/ScubaLog.js
+++ src/components/ScubaLog.js
@@ -1,13 +1,31 @@
 import React from "react"
 import { ApplicationViews } from "./ApplicationViews"
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 
 export const ScubaLog = () =>(
-    <>
-    <img src="logo.png" />
-    <h2>Dashboard</h2>
-  <ApplicationViews />
+
+<>
+<Route render={() => {
+    if (localStorage.getItem("activeUser")) {
+        return (
+            <>
+                <img alt="logo" />
+                <h2>Dashboard</h2>
+                <Route render={props => <ApplicationViews {...props} />} />
+            </>
+        )
+    } else {
+        return <Redirect to="/login" />
+    }
+}} />
+
+<Route path="/login" render={props => <Login {...props} />} />
+<Route path="/register" render={props => <Register {...props} />} />
 </>
+
 )
 
 
