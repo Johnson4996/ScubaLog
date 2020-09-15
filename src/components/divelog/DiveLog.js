dd401c9fe1ddcba7142e8e9163de1f0ebf6972b7
Johnson4996 <Johnson.4996@gmail.com>
2020-09-15 16:24:19 -0400

    details rendering when log is clicked

--- src/components/divelog/DiveLog.js
+++ src/components/divelog/DiveLog.js
@@ -2,8 +2,29 @@ import React from "react"
 
 export const DiveLog = ({diveLog}) =>{
 
-    <article>
-        
+return(
+    <article className="diveLogDetails">
+        <p>{diveLog.title}</p>
+        <p>{diveLog.date}</p>
+        <p>{diveLog.location}</p>
+        <p>{diveLog.surfaceInterval}</p>
+        <p>{diveLog.pressureGroupEnter}</p>
+        <p>{diveLog.pressureGroupExit}</p>
+        <p>{diveLog.bottomTime}</p>
+        <p>{diveLog.maxDepth}</p>
+        <p>{diveLog.avgDepth}</p>
+        <p>{diveLog.airEnter}</p>
+        <p>{diveLog.airExit}</p>
+        <p>{diveLog.tankSize}</p>
+        <p>{diveLog.tankTypeId}</p>
+        <p>{diveLog.weight}</p>
+        <p>{diveLog.suitTypeId}</p>
+        <p>{diveLog.weatherConditions}</p>
+        <p>{diveLog.comments}</p>
+        <p>{diveLog.verifyName}</p>
+        <p>{diveLog.verifyCert}</p>
+        <p>{diveLog.verifyType}</p>
     </article>
+)
 }
     
--- src/components/divelog/DiveLogDetail.js
+++ src/components/divelog/DiveLogDetail.js
@@ -1,9 +1,18 @@
-import React,{useContext, useEffect} from "react"
+import React,{useContext} from "react"
 import {LogContext} from "./DiveLogProvider"
+import { DiveLog } from "./DiveLog"
 
 
-export const DiveLogDetail = () =>{
-const 
+export const DiveLogDetail = (props) =>{
+    const {diveLogs} = useContext(LogContext)
+
+    const chosenDiveLog = parseInt(props.match.params.diveLogId)
+    const diveLog = diveLogs.find(l => l.id === chosenDiveLog) || {}
+
+    return(
+        <DiveLog key={diveLog.id} diveLog={diveLog} />
+    )
+   
 
 
 }
