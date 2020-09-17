4e0297b1e82729b8dcdc5886cc398985de583e7a
Johnson4996 <Johnson.4996@gmail.com>
2020-09-17 16:11:28 -0400

    log details now displays name of suit type and name of tank type instead of id

--- src/components/divelog/DiveLog.js
+++ src/components/divelog/DiveLog.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom"
+import {SuitContext} from "../suittype/SuitTypeProvider"
+import {TankContext} from "../tanktype/TankTypeProvider"
 
 export const DiveLog = ({ diveLog }) => {
+    const {suitTypes} = useContext(SuitContext)
+    const {tankTypes} = useContext(TankContext)
+
+    const foundSuit = suitTypes.map(s =>  s.id === diveLog.suitTypeId ? s.name: "" )
+    const foundTank = tankTypes.map(t =>  t.id === diveLog.tankTypeId ? t.name: "" )
+    
     return (
         <>
             <Link className="log-link"
@@ -21,9 +29,9 @@ export const DiveLog = ({ diveLog }) => {
                 <p>{diveLog.airEnter}</p>
                 <p>{diveLog.airExit}</p>
                 <p>{diveLog.tankSize}</p>
-                <p>{diveLog.tankTypeId}</p>
+                <p>{foundTank}</p>
                 <p>{diveLog.weight}</p>
-                <p>{diveLog.suitTypeId}</p>
+                <p>{foundSuit}</p>
                 <p>{diveLog.weatherConditions}</p>
                 <p>{diveLog.comments}</p>
                 <p>{diveLog.verifyName}</p>
