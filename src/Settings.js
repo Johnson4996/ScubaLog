338929ded85cda321b1b60e5c969ba5619233b63
Johnson4996 <Johnson.4996@gmail.com>
2020-09-23 11:07:29 -0500

    got lat and long finally, working on getting map rendered

--- src/Settings.js
+++ src/Settings.js
@@ -1,3 +1,12 @@
+export default {
+    zoomLevel: 16,
+    latitudeForFocusingMap: 51.4934,
+    longitudeForFocusingMap: 0.0098,
+    markerLongitude: 51.4934,
+    markerLatitude: 0.0098,
+    apiKey: 'AIzaSyAmCkZ0Bi3zHwcxTWRutE0aypAHghsknPE',
+    useDefaultUI: false,
+    googleMapsMarkerIcon: 'marker icon link'
+};
 
 
-//15GIdwKGGHkhCAaAJAI4xjFSzEAoU0ct - mapquest api key
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,18 +1,28 @@
-import React, { useContext } from "react"
-import {GeoCodeContext} from "./GeoCodeProvider"
+import React, { useContext, useEffect, useState } from "react"
 import {LogContext} from "../divelog/DiveLogProvider"
+import {Map} from "mapbox-gl"
 
-export const MapRender = () =>{
+
+export const MapRender = (props) =>{
     const {diveLogs} = useContext(LogContext)
-    const {latLong, getLatLong} = useContext(GeoCodeContext)
+    const [latLong, setLatLong] = useState([])
+    const []
     
+    let allLatLong = []
 
-    diveLogs.map(dl =>{
-        //getLatLong(dl.location)
-        //console.log(latLong) -- returns an endless log of api calls
-    })
+    useEffect(()=>{
+        diveLogs.map(dl =>{
+            // return fetch(`https://api.opencagedata.com/geocode/v1/json?q=${dl.location}&key=7706b1cbc9874da9a15331f9bf55b3f2&limit=1`)
+            //     .then(res => res.json())
+            //     .then(parsedRes => setLatLong(parsedRes.results[0].geometry))
+            
+            
+        })
+    },[diveLogs])
 
-    
+   
+    return (
+        <Map>
 
-    return <h2>GeoCode Render</h2>
-}
+        </Map>
+    )
