b93b6d71e99f41d9cc18eaa0bd8e90c955d82ec0
Johnson4996 <Johnson.4996@gmail.com>
2020-09-23 09:12:44 -0500

    initial file structure

---
+++ src/components/pictures/PictureFeedProvider.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export const PictureFeedProvider = () =>{
+
+const handleImageUpload = (images) =>{
+    const formData = new FormData()
+    formData.append('file', files[0])
+    formData.append('ml_default', )
+
+}
+
+
+}
