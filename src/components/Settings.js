791087be829c534f172dfdabd6cb1851a2ee3053
Johnson4996 <Johnson.4996@gmail.com>
2020-09-23 15:01:45 -0500

    got one marker adding, still need to figure out how to display multiple

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,12 +1,5 @@
 export default {
-    zoomLevel: 16,
-    latitudeForFocusingMap: 51.4934,
-    longitudeForFocusingMap: 0.0098,
-    markerLongitude: 51.4934,
-    markerLatitude: 0.0098,
     apiKey: 'AIzaSyCB_EW3RlzQLyuQNgVN0ShLqcoCrEK2ptI',
-    useDefaultUI: false,
-    googleMapsMarkerIcon: 'marker icon link'
 };
 
 
--- src/components/map/MapRender.js
+++ src/components/map/MapRender.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
 import Settings from "../Settings"
 import mapSyles from "./MapStyles"
+import { LogContext } from '../divelog/DiveLogProvider';
 
 export const MapRender =(props) => {
+    const {diveLogs} = useContext(LogContext)
+    const [latLong, setLatLong] = useState([])
+    
+    useEffect(()=>{
+        diveLogs.map(dl =>{
+            return fetch(`https://api.opencagedata.com/geocode/v1/json?q=${dl.location}&key=7706b1cbc9874da9a15331f9bf55b3f2&limit=1`)
+                .then(res => res.json())
+                .then(parsedRes => setLatLong([parsedRes.results[0].geometry]))
+            
+            
+        })
+    },[diveLogs])
+
+
+
+
 
    const {isLoaded, loadError} = useLoadScript({
        googleMapsApiKey: Settings.apiKey
@@ -25,7 +42,7 @@ export const MapRender =(props) => {
    }
 
    
-
+  
    if(loadError) console.log("error loading maps")
    if(!isLoaded) return "Loading..."
     
@@ -36,7 +53,15 @@ export const MapRender =(props) => {
             options = {options}
              zoom={1}
              center={center}
-            ></GoogleMap>
+            >
+                {
+                    latLong.map(l =>(
+                     <Marker key={l.lat}
+                         position ={{lat: l.lat, lng: l.lng}} 
+                         />
+                    ))
+                }
+            </GoogleMap>
         </div>
     )
   }
