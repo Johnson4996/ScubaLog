6b410daf1a5f802aeb785c1717cdef05df349689
Johnson4996 <Johnson.4996@gmail.com>
2020-12-03 11:06:48 -0500

    Updates

---
+++ precache-manifest.8e0ccf3cf0b0c6026310b837dfcfaf5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f086a526502631898a3d2a62e0148d50",
+    "url": "/ScubaLog/index.html"
+  },
+  {
+    "revision": "98bbf91a205af5ecbbe3",
+    "url": "/ScubaLog/static/css/main.27a61d0d.chunk.css"
+  },
+  {
+    "revision": "a8b4ca4271696d1de1ff",
+    "url": "/ScubaLog/static/js/2.5afbee7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ScubaLog/static/js/2.5afbee7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98bbf91a205af5ecbbe3",
+    "url": "/ScubaLog/static/js/main.068c5ba4.chunk.js"
+  },
+  {
+    "revision": "9f940539aca485d8218c",
+    "url": "/ScubaLog/static/js/runtime-main.27e16c63.js"
+  },
+  {
+    "revision": "2f5bc5691af86afe990f550ab263e159",
+    "url": "/ScubaLog/static/media/LogImg.2f5bc569.png"
+  }
+]);
